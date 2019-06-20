5e80afbe37a5e2dcdd7789cedd9e34c1afb2b92a
alexbertis <alexberti02@gmail.com>
2019-06-20 17:49:38 +0200

    Mapa y actividad

---
+++ app/src/main/java/com/brontapps/climatefight/ClimateApp.java
@@ -0,0 +1,11 @@
+package com.brontapps.climatefight;
+
+import android.app.Application;
+
+
+public class ClimateApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/com/brontapps/climatefight/MapaInfoActivity.java
@@ -0,0 +1,51 @@
+package com.brontapps.climatefight;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.FragmentActivity;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+
+
+public class MapaInfoActivity extends FragmentActivity {
+
+    private MapView map = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mapa_info);
+
+        map = findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
+
+        IMapController mapController = map.getController();
+        mapController.setZoom(9.5);
+        GeoPoint startPoint = new GeoPoint(42.1401, -0.408897);
+        mapController.setCenter(startPoint);
+
+    }
+
+
+    public void onResume(){
+        super.onResume();
+        //this will refresh the osmdroid configuration on resuming.
+        //if you make changes to the configuration, use
+        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
+        map.onResume(); //needed for compass, my location overlays, v6.0.0 and up
+    }
+    public void onPause(){
+        super.onPause();
+        //this will refresh the osmdroid configuration on resuming.
+        //if you make changes to the configuration, use
+        //SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        //Configuration.getInstance().save(this, prefs);
+        map.onPause();  //needed for compass, my location overlays, v6.0.0 and up
+    }
+}
