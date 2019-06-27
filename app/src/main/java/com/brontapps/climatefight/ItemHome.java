6c08d7c6f0d7cd3e311c0fff56eb7be5d3f73953
alexbertis <alexberti02@gmail.com>
2019-06-27 18:22:06 +0200

    ItemHome

---
+++ app/src/main/java/com/brontapps/climatefight/ItemHome.java
@@ -0,0 +1,9 @@
+package com.brontapps.climatefight;
+
+public class ItemHome {
+
+    private String nombre;
+    public ItemHome(String nombre){
+        this.nombre = nombre;
+    }
+}
--- app/src/main/java/com/brontapps/climatefight/MainSharedViewModel.java
+++ app/src/main/java/com/brontapps/climatefight/MainSharedViewModel.java
@@ -2,6 +2,9 @@ package com.brontapps.climatefight;
 
 import androidx.lifecycle.ViewModel;
 
+import java.util.List;
+
 public class MainSharedViewModel extends ViewModel {
     // TODO: Implement the ViewModel
+    public List<ItemHome> itemList;
 }
